6b2a56e8935450bc99690742372296d733f5dc51
roos-hva <90178913+roos-hva@users.noreply.github.com>
2021-10-05 11:24:20 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,30 @@
 // JavaScript Document
-var deButton = document.querySelector("nav button");
+var burgerMenu = document.querySelector("header nav button");
 
-deButton.addEventListener("click", openMenu);
+burgerMenu.addEventListener("click", openMenu);
+
+burgerMenu.addEventListener("focus", openMenu);
 
 function openMenu() {
   document.body.classList.toggle("menuOpen");
 }
+
+var eersteLink = document.querySelector("main a:first-of-type");
+
+
+eersteLink.addEventListener("focus", closeMenu);
+
+function closeMenu() {
+  document.body.classList.remove("menuOpen");
+}
+
+// forEach functie gemaakt met hulp van Rowin
+
+var knopGeluid = document.querySelectorAll("a");
+
+knopGeluid.forEach(function(elem) {
+  elem.addEventListener("click", function() {
+    var audio = new Audio('geluid/mixkit-pebbles-click-1128.mp3');
+    audio.play();  });
+});
+
